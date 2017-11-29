32e215fcded662d5ca69dfd4e9823ab1e75be2d7
mihail.yartsev <mihail.yartsev@gmail.com>
2017-11-29 18:39:48 +0300

    LWDEV-3705 More unit-test cases from Alexander Popov

--- tests/Tests/Services/OutliersOrderbooksServiceTests.cs
+++ tests/Tests/Services/OutliersOrderbooksServiceTests.cs
@@ -12,7 +12,8 @@ namespace Tests.Services
 {
     public class OutliersOrderbooksServiceTests
     {
-        private static readonly TestSuit<OutliersOrderbooksService> _testSuit = TestSuit.Create<OutliersOrderbooksService>();
+        private static readonly TestSuit<OutliersOrderbooksService> _testSuit =
+            TestSuit.Create<OutliersOrderbooksService>();
 
         [TearDown]
         public void TearDown()
@@ -20,23 +21,64 @@ namespace Tests.Services
             _testSuit.Reset();
         }
 
-        private static IEnumerable<TestCaseData> GetCasesForPrices(decimal[] prices, params (decimal Threshold, int[] Result)[] others)
+        private static IEnumerable<TestCaseData> GetCasesForPrices(decimal[] prices,
+            params (decimal Threshold, int[] Result)[] others)
         {
             foreach (var (threshold, result) in others)
                 yield return new TestCaseData(prices, threshold).Returns(result);
         }
-        
-        public static IEnumerable<TestCaseData> GetCases() {
 
-            return new [] {
+        public static IEnumerable<TestCaseData> GetCases()
+        {
+            return new[]
+            {
                 // last exchange encompassing all, odd exchanges count
                 GetCasesForPrices(new decimal[] {100, 101, 102, 103, 100, 103, 99, 101, 70, 110},
-                    (0.02m, new[] {2, 5}), (0.03m, new[] {5}), (0.3m, new[] {5}), (0.301m, new int[0])),
+                    (0.02m, new[] {2, 5}),
+                    (0.03m, new[] {5}),
+                    (0.3m, new[] {5}),
+                    (0.301m, new int[0])),
+
                 GetCasesForPrices(new decimal[] {100, 101, 102, 103, 100, 103, 99, 101, 110, 111},
-                    (0.019m, new[] {1, 2, 4, 5}), (0.02m, new[] {2, 5}), (0.021m, new[] {5}), (0.10m, new[] {5}), (0.101m, new int[0])),
+                    (0.019m, new[] {1, 2, 4, 5}),
+                    (0.02m, new[] {2, 5}),
+                    (0.021m, new[] {5}),
+                    (0.10m, new[] {5}),
+                    (0.101m, new int[0])),
+
                 GetCasesForPrices(new decimal[] {100, 101, 102, 103, 100, 103, 99, 101, 50, 51},
-                    (0.50m, new[] {5}), (0.501m, new int[0])),
-                }.SelectMany(c => c);
+                    (0.50m, new[] {5}),
+                    (0.501m, new int[0])),
+
+                GetCasesForPrices(new decimal[] {150, 152, 100, 103, 149, 163, 111, 116, 89, 99},
+                    (0.11m, new[] {1, 2, 3, 5}),
+                    (0.12m, new[] {1, 3, 5}),
+                    (0.19m, new[] {1, 3, 5}),
+                    (0.2m, new[] {1, 3}),
+                    (0.35m, new[] {1, 3}),
+                    (0.36m, new[] {3}),
+                    (0.4m, new[] {3}),
+                    (0.41m, new int[0])),
+
+                GetCasesForPrices(new decimal[] {100, 200, 110, 170, 140, 160, 100, 110, 107, 109},
+                    (0.175m, new[] {1, 3, 4, 5}),
+                    (0.18m, new[] {3, 4, 5}),
+                    (0.27m, new[] {3, 4, 5}),
+                    (0.28m, new[] {4, 5}),
+                    (0.35m, new[] {4, 5}),
+                    (0.355m, new[] {5}),
+                    (0.36m, new int[0])),
+
+                GetCasesForPrices(new[] {150, 150, 152, 151.95m, 146, 151, 150, 157, 155, 170},
+                    (0.012m, new[] {1, 3, 4, 5}),
+                    (0.014m, new[] {3, 4, 5}),
+                    (0.032m, new[] {3, 4, 5}),
+                    (0.034m, new[] {3, 5}),
+                    (0.038m, new[] {3, 5}),
+                    (0.04m, new[] {5}),
+                    (0.115m, new[] {5}),
+                    (0.12m, new int[0])),
+            }.SelectMany(c => c);
         }
 
         [TestCaseSource(nameof(GetCases))]
