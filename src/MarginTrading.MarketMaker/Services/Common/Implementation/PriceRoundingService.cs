d9b74b39700df54ca6f0078dad7044d188dc8369
mihail.yartsev <mihail.yartsev@gmail.com>
2018-01-12 12:46:16 +0300

    LWDEV-3996 log fixes

--- src/MarginTrading.MarketMaker/Services/Common/Implementation/PriceRoundingService.cs
+++ src/MarginTrading.MarketMaker/Services/Common/Implementation/PriceRoundingService.cs
@@ -1,31 +1,20 @@
 ﻿using System;
-using Common;
-using Common.Log;
 
 namespace MarginTrading.MarketMaker.Services.Common.Implementation
 {
     internal class PriceRoundingService : IPriceRoundingService
     {
         private readonly IAssetPairsInfoService _assetPairsInfoService;
-        private readonly ILog _log;
 
-        public PriceRoundingService(IAssetPairsInfoService assetPairsInfoService, ILog log)
+        public PriceRoundingService(IAssetPairsInfoService assetPairsInfoService)
         {
             _assetPairsInfoService = assetPairsInfoService;
-            _log = log;
         }
 
         public Func<decimal, decimal> GetRoundFunc(string assetPairId)
         {
-            var accuracy = _assetPairsInfoService.Get(assetPairId)?.Accuracy;
-            if (accuracy == null)
-            {
-                _log.WriteWarningAsync(nameof(PriceRoundingService), new {AssetPairId = assetPairId}.ToJson(),
-                    $"Accuracy for assetPair {assetPairId} was not found");
-                return p => p;
-            }
-                    
-            return p => Math.Round(p, accuracy.Value);
+            var accuracy = _assetPairsInfoService.Get(assetPairId)?.Accuracy ?? 8;
+            return p => Math.Round(p, accuracy);
         }
     }
 }
