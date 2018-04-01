8e1cb960f868f3fa8d3f56f6fde92fee53d8cd7f
mihail.yartsev <mihail.yartsev@gmail.com>
2018-04-02 00:15:31 +0300

    LWDEV-6610 Use backend asset pairs dictionary instead of AssetService

--- src/MarginTrading.MarketMaker/Settings/DbSettings.cs
+++ src/MarginTrading.MarketMaker/Settings/DbSettings.cs
@@ -1,9 +1,14 @@
-﻿namespace MarginTrading.MarketMaker.Settings
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace MarginTrading.MarketMaker.Settings
 {
     public class DbSettings
     {
+        [AzureTableCheck]
         public string ConnectionString { get; set; }
+        [AzureTableCheck]
         public string LogsConnString { get; set; }
+        [AzureBlobCheck]
         public string QueuePersistanceRepositoryConnString { get; set; }
     }
 }
