2d455b70d928f26aaac8ea3d0bd47936be3d618f
mihail.yartsev <mihail.yartsev@gmail.com>
2018-04-04 11:47:10 +0300

    LWDEV-6645 fix asset pairs cache to tolerate external service fails

--- tests/Tests/Integrational/Controllers/CrossRateCalcInfosControllerTests.cs
+++ tests/Tests/Integrational/Controllers/CrossRateCalcInfosControllerTests.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Immutable;
-using System.Linq;
-using Autofac;
+﻿using Autofac;
 using FluentAssertions;
-using Lykke.Service.Assets.Client.Models;
-using MarginTrading.MarketMaker.Contracts.Enums;
-using MarginTrading.MarketMaker.Contracts.Models;
 using MarginTrading.MarketMaker.Controllers;
-using MarginTrading.MarketMaker.Enums;
 using NUnit.Framework;
 
 namespace Tests.Integrational.Controllers
@@ -30,58 +23,6 @@ namespace Tests.Integrational.Controllers
             
             //assert
             info.Should().BeNull();
-        }
-        
-        [Test]
-        public void OnPairCreation_ShouldAutogenerateCrossRatesCalcInfo()
-        {
-            //arrange
-            var env = _testSuit.Build();
-            env.Setup(b => b.RegisterType<AssetPairsController>().AsSelf());
-            env.Setup(b => b.RegisterType<CrossRateCalcInfosController>().AsSelf());
-            var container = env.CreateContainer();
-            var assetPairsController = container.Resolve<AssetPairsController>();
-            var crossRateCalcInfosController = container.Resolve<CrossRateCalcInfosController>();
-            env.AssetPairs.Add(new AssetPair
-            {
-                Id = "pair",
-                BaseAssetId = "base",
-                QuotingAssetId = "quoting",
-                Source = "src1",
-                Source2 = "src2"
-            });
-            env.AssetPairs.Add(new AssetPair
-            {
-                Id = "src1",
-                BaseAssetId = "base-src1",
-                QuotingAssetId = "pair"
-            });
-            env.AssetPairs.Add(new AssetPair
-            {
-                Id = "src2",
-                BaseAssetId = "pair",
-                QuotingAssetId = "quoting-src2"
-            });
-
-            //act
-            assetPairsController.Add("pair", AssetPairQuotesSourceTypeDomainEnum.Disabled);
-            var info = crossRateCalcInfosController.Get("pair");
-            
-            //assert
-            info.Should().BeEquivalentTo(new CrossRateCalcInfoModel
-            {
-                ResultingPairId = "pair",
-                Source1 = new CrossRateSourceAssetPairModel
-                {
-                    Id = "src1",
-                    IsTransitoryAssetQuoting = true,
-                },
-                Source2 = new CrossRateSourceAssetPairModel
-                {
-                    Id = "src2",
-                    IsTransitoryAssetQuoting = false,
-                },
-            });
         }
     }
 }
